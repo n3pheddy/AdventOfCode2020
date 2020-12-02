f87c497d4f40b504babb765f5810bca0b60834eb
Edison Wang <ediswang@microsoft.com>
2020-12-02 02:36:17 -0800

    Solution for day 1 (2 stars)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AdventOfCode2020
 Solutions for Advent of Code 2020 https://adventofcode.com/2020
+
+1. python aoc1.py  
+Addends = 2 -> 942 + 1078 = 2020, 942 * 1078 = 1015476  
+Addends = 3 -> 956 + 802 + 262 = 2020, 956 * 802 * 262 = 200878544
---
+++ aoc1.py
@@ -0,0 +1,63 @@
+from functools import reduce
+from operator import mul
+
+def find_addends(sum, input, level):
+  """
+  Given a target sum and a list of possible combinations, enumerate input
+  and look for addends that return the given sum. level=0 indicates 2 addends,
+  level=1 indicates 1 addends, and so on. Returns a list of its result.
+
+  Time complexity : O(m*n)
+  Space complexity: O(m*n)
+  Where m = len(input), n = level+1
+
+  It's possible to get O(m*nlogn) by sorting the list first, then generate an
+  array of {sum} elements for an O(1) lookup.
+  """
+  candidates = []
+
+  for addend in input:
+    remainder = sum - addend
+
+    if level == 0:
+      if addend in candidates:
+        return [addend, remainder]
+      else:
+        candidates.append(remainder)
+    else:
+      input.remove(addend)
+      next_remainder = find_addends(remainder, input, level-1)
+
+      if next_remainder:
+        next_remainder.insert(0, addend)
+        return next_remainder
+
+def find_solution(sum, input, level):
+  """
+  Given a target sum and a list of possible combinations, find
+  a solution and print its result.
+  """
+  result = find_addends(sum, input, level)
+
+  if result:
+    sresult = list(map(str, result))
+    product = reduce(mul, result)
+
+    print(f'Addends = {level+2} -> {" + ".join(sresult)} = {sum}, ' +
+          f'{" * ".join(sresult)} = {product}')
+  else:
+    print('No solution!')
+
+if __name__ == '__main__':
+  sum = 2020
+  input = []
+
+  with open('puzzle_input.txt') as f:
+    line = f.readline()
+    while line:
+      input.append(int(line))
+      line = f.readline()
+
+  # Use level=0 to find 2 operands, level=1 to find 3 operands, etc.
+  for level in range(0, 2):
+    find_solution(sum, input.copy(), level)
